a8ad79e538514ff75cb55f8c775d4491f3d36148
Muhammad Tabish Khanday <61789893+mtabishk@users.noreply.github.com>
2022-09-29 15:44:37 +0530

    Update exp2_perform_operations.sh

--- exp2_perform_operations.sh
+++ exp2_perform_operations.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "Enter a: "
+echo "Enter a "
 read a
 
-echo "Enter b: "
+echo "Enter b "
 read b
 
+echo "Enter op"
+
 case $op in
-  +)c = `expr $a + $b`;;
-  -)c = `expr $a - $b ;;
-  /)c = `expr $a/$b ;;
-  \*)c = `expr $a * $b ;;
+  +)c=`expr "$a + $b"`
+  ;;
+  -)c=`expr "$a - $b"`
+  ;;
+  /)c=`expr "$a / $b" | bc`
+  ;;
+  \*)c=`expr "$a * $b" | bc -l`
+  ;;
 esac
 
-echo "Result: "
+echo "Result "
 echo $c
